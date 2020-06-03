5a472dbeb5b2e73f1c76483ed8fe445627f7c99d
Alex Santos de Souza <alexssmusica@gmail.com>
2020-06-03 14:21:15 -0300

    add kenex
    adicionado kenex, ajustado conexao com banco de dados , criado arquivo de rotas...

---
+++ server/src/routes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const routes  = express.Router()
+
+
+routes.get('/', (req, res) => {
+
+    return res.json({message: 'Olá'})
+
+});
+
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get('/users', (req, res) => {
-
-    res.json([
-        'Alex',
-        'Santos',
-        'Souza'
-    ])
-});
-
-
-
+app.use(express.json())
+app.use(routes)
 app.listen(3333);
 
