47fc010016b258fc0f63242743b3d0bfcab04b39
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2020-06-13 10:18:08 -0300

    Update .eslintrc.js

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -4,11 +4,6 @@ module.exports = {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
   },
-  settings: {
-    react: {
-      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
-    }
-  },
   extends: [// Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
